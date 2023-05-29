f91fe1ea2fcd053ffc27c4a36cccc5541fa817bb
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-28 22:31:59 -0300

    feat: adicona icons

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,22 @@
 import styles from "./hero.module.css"
+import { LinkedinLogo, GithubLogo, FileText } from "@phosphor-icons/react"
 
 export function Hero() {
     return (
         <main className={styles.hero}>
             <div className="container">
                 <div className={styles.hero_content}>
-                    <p>Oi, eu sou o Israel </p>
+                    <p>Oi, eu sou o Israel</p>
                     <h2>REACT</h2>
                     <h2>DEVELOPER</h2>
                     <p>Tenho 28 anos sou desenvolvedor frontend com 1 ano de <br /> experiência trabalhando como freelancer</p>
+                    <div className={styles.content_links}>
+
+                        <a href=""><LinkedinLogo size={32} /></a>
+                        <a href=""><GithubLogo size={32} /></a>
+                        <a href=""><FileText size={32} /></a>
+
+                    </div>
                 </div>
             </div>
         </main>
