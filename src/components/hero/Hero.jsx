f5bc07abdd9a29b7eb3257128386263ad74045db
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-28 22:11:45 -0300

    feat: adiciona:  font family

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,14 +3,14 @@ import styles from "./hero.module.css"
 export function Hero() {
     return (
         <main className={styles.hero}>
-
             <div className="container">
                 <div className={styles.hero_content}>
-
+                    <p>Oi, eu sou o Israel </p>
+                    <h2>REACT</h2>
+                    <h2>DEVELOPER</h2>
+                    <p>Tenho 28 anos sou desenvolvedor frontend com 1 ano de <br /> experiência trabalhando como freelancer</p>
                 </div>
-
             </div>
-
         </main>
     )
 }
