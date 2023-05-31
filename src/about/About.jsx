f5c5c25d53ffa0848f1b4d40e7d5e76309d7a45b
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-31 19:06:47 -0300

    feat: adiciona avatar e estilo

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,10 +1,31 @@
 import styles from "./about.module.css";
+import avatar from "../assets/img_profile.png";
+
 
 export function About() {
 
     return (
-        <section>
+        <section className={styles.about}>
             <div className="container">
+                <div className={styles.about_content}>
+
+                    <div className={styles.about_img}>
+                        <div className={styles.about_img_container}>
+
+                            <img src={avatar} alt="" />
+
+                        </div>
+
+                    </div>
+
+                    <div className={styles.about_info}>
+                        <div>
+
+                        </div>
+
+                    </div>
+
+                </div>
 
             </div>
 
