5ac3523f92c0891765b80dd25849c9653a117355
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-30 22:51:34 -0300

    feat: adiciona funcionalidade abrir/fechar menu mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,57 @@
+import { useState } from "react"
 import styles from "./navbar.module.css"
+import { List, X } from "@phosphor-icons/react"
+
 
 export function Navbar() {
+
+    const [active, setActive] = useState(false);
+
+    const toggleMenu = () => {
+        setActive(!active);
+
+    };
+
+
     return (
         <section className={styles.navbar}>
+
             <div className="container">
-                <div className={styles.nav}>
-                    <nav>
-                        <ul>
-                            <li>Home</li>
-                            <li>Projetos</li>
-                            <li>About</li>
-                        </ul>
-                    </nav>
+
+                <div className={styles.nav_content}>
+
+                    <div className={`${styles.nav} ${active ? styles.activeMenu : styles.hidden}`}>
+
+                        <button
+                            className={styles.iconCloseMenu}
+                            onClick={() => toggleMenu()}
+                        >
+                            <X size={40} />
+                        </button>
+
+
+
+                        <nav>
+                            <ul>
+                                <li>Home</li>
+                                <li>Projetos</li>
+                                <li>About</li>
+                            </ul>
+                        </nav>
+                    </div>
+
+
+                    <button
+                        className={styles.iconOpenMeunu}
+                        onClick={() => toggleMenu()}
+                    >
+                        <List size={40} />
+                    </button>
+
                 </div>
+
             </div>
+
         </section>
     )
 }
