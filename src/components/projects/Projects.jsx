0f499d318aa119d15b21a5fe99b04a7a425dcde7
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-30 23:23:05 -0300

    feat: adiciona component Projects

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import styles from "./projects.module.css";
+
+export function Projects() {
+    return (
+        <section className={styles.projects_section}>
+            <div className="container">
+                <div className={styles.projects_content}>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
