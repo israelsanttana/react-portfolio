0ec67c39308d13e61a34f6982bee64d9116c679b
israelsanttana <israelsantanasantos7@gmail.com>
2023-06-10 02:45:03 -0300

    feat: adiciona texto about

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,7 +1,11 @@
 import styles from "./about.module.css";
 import avatar from "../assets/img_profile.png";
-import { FileJs, FileHtml, FileCss, FileTs } from "@phosphor-icons/react"
-import { FaNode, FaReact, FaBootstrap } from "react-icons/fa"
+import iconHtml from "../assets/html.png"
+import iconCss from "../assets/css.png"
+import iconJavascript from "../assets/javascript.png"
+import iconTypescript from "../assets/typescript.png"
+import iconNode from "../assets/nodejs.png"
+import iconReact from "../assets/react.png"
 
 
 export function About() {
@@ -17,48 +21,43 @@ export function About() {
                             <img src={avatar} alt="" />
 
                         </div>
+                        <div className={styles.icons}>
+
+                            <img src={iconHtml} alt="" />
+                            <img src={iconCss} alt="" />
+                            <img src={iconJavascript} alt="" />
+                            <img src={iconTypescript} alt="" />
+                            <img src={iconNode} alt="" />
+                            <img src={iconReact} alt="" />
+
+                        </div>
 
                     </div>
 
                     <div className={styles.about_info}>
                         <div>
 
-                            <h2>Skills</h2>
+                            <span>
+                                Sou apaixonado por resolver problemas e criar soluções inovadoras. Com uma sólida experiência em tecnologias como HTML, CSS, JavaScript, TypeScript, React, Next.js e Node.js, estou pronto para enfrentar qualquer desafio que envolva o desenvolvimento de interfaces e aplicativos web.
+                                <br />
+                                <br />
+                                Durante a minha trajetória, tenho trabalhado tanto como freelancer quanto em projetos pessoais. Essas oportunidades me permitiram aprimorar minhas habilidades e me destacar na construção de websites elegantes, intuitivos e responsivos. Tenho um olhar atento aos detalhes e estou sempre em busca da melhor experiência do usuário.
+                                <br />
+                                <br />
 
-                            <p>
-                                Desenvolvimento de websites e SPA's (Single Page Application) com Javascript, HTML, CSS, React.js, Bootstrap 5, tailwind CSS e styled-components.
-                            </p>
-                            <p>
-                                Desenvolvimento de sistemas com Typescript, NextJS e banco de dados
-                            </p>
-                            <p>
-                                Consumo de APIs REST/RESTful
-                            </p>
-                            <p>
-                                Melhorias de UI/UX dos produtos digitais da empresa.
-                            </p>
-                            <p>
-                                Versionamento de código com GIT
-                            </p>
-                            <p>
-                                Desenvolvimento de e-commerce através de CMS com experiência em Wordpress
+                                Além disso, sou capaz de trabalhar em pojetos com consumo de APIs e manipulação de bancos de dados.
+                                <br />
+                                <br />
 
-                            </p>
-                        </div>
+                                Minha paixão por resolver problemas impulsiona meu trabalho. Adoro mergulhar em desafios complexos e encontrar soluções elegantes.
+                                <br />
+                                <br />
 
-                        <div>
+                                Através da minha experiência com React e Next.js, tenho construído aplicações de alta qualidade, aproveitando ao máximo as vantagens dessas tecnologias modernas. Sou capaz de criar interfaces dinâmicas e interativas, com recursos como roteamento e validações de formularios.
 
-                            <h2>Tecnologias</h2>
-
-                            <span><FileHtml size={32} /></span>
-                            <span><FileCss size={32} /></span>
-                            <span><FileJs size={32} /></span>
-                            <span><FileTs size={32} /></span>
-                            <span><FaNode size={32} /></span>
+                            </span>
 
                         </div>
-
-
 
                     </div>
 
